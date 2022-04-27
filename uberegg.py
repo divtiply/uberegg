fd855d2d33df5bfe97b91026b4092dd886e06bca
李全城 <398208587@qq.com>
2022-04-27 18:04:43 +0800

    添加 本地依赖安装模式 增加速度 不然每次都下载依赖 解压实在太慢了

--- uberegg.py
+++ uberegg.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import tempfile
 from distutils import log
 
 from setuptools.command.bdist_egg import bdist_egg
@@ -42,7 +43,13 @@ class bdist_uberegg(bdist_egg):
 
     @staticmethod
     def _install(requirements, target):
-        return subprocess.check_call(
+        # mark local cache 
+        cache_dir = os.path.join(tempfile.gettempdir(),'.uberegg.cache')
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir, 755)
+        # using  local cache  install requirements 
+        # https://pip.pypa.io/en/latest/user_guide/#installing-from-local-packages
+        return  subprocess.check_call([sys.executable, '-m', 'pip', 'download'] + ['--destination-directory', cache_dir, '-r', requirements]) and subprocess.check_call(
             [sys.executable, '-m', 'pip', 'install']
-            + ['-U', '-t', target, '-r', requirements]
+            + ['--no-index', '--find-links=%s' % cache_dir,'-t', target, '-r', requirements]
         )
